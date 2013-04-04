98c78d7d8374303bed6d72ee97933bf257d208ad
Sam Nunnery <nunnerys14@mycu.concord.edu>
2013-04-04 13:22:50 -0700

    Create Wall.java

---
+++ Wall.java
@@ -0,0 +1,13 @@
+package textbased;
+
+//Objects karel cannot move on top of
+public class Wall extends Entity
+{
+    //constructor
+    public Wall(int x,int y)
+    {
+        super(x,y);
+    }
+    
+    //Add image code to this class later
+}
