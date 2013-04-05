0c07ab6c9d0c5d16cad416702511c72026c4e71c
Sam Nunnery <nunnerys14@mycu.concord.edu>
2013-04-05 14:37:33 -0300

    Create Gem.java
    Creation of gem class

---
+++ Gem.java
@@ -0,0 +1,13 @@
+package textbased;
+
+//Objects karel can pick up
+public class Gem extends Entity
+{
+    //constructor
+    public Gem(int x,int y)
+    {
+        super(x,y);
+    }
+    
+    //Add image code to this class later
+}
